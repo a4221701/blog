1e07c867b9855547ea0f8555fc1eb329402a6eb5
MyPJGitHub <1464847406@qq.com>
2021-03-11 01:57:23 +0800

    测试12

--- src/main/java/com/ssm/ssmMaven/controller/TestController.java
+++ src/main/java/com/ssm/ssmMaven/controller/TestController.java
@@ -42,7 +42,6 @@ public class TestController {
         modelAndView.setViewName("list");
         System.out.println("aa");
         System.out.println("bbb");
-        System.out.println("ccc");
         return modelAndView;
     }
 }
